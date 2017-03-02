9e284a0cd1610c125bee6b0706fb9a2070a640ce
gustavo <gustavo>
2017-03-01 21:42:22 -0300

    Minor updates to table_services source

--- test/test.c
+++ test/test.c
@@ -6,7 +6,11 @@
 #include "table_services.h"
 
 int main(int argc, char** argv) {
-  ctApdu_t m;
-  printf("%zu\n", sizeof m);
+  uint8_t p[] = {1,2,4,6,8,10,15};
+  ctApdu_t m = ctFullWriteRequest(0xabcd, sizeof p, p);
+  int i;
+  for(i = 0; i <= 5 + sizeof p; ++i)
+    printf("%x ", m.apdu[i]);
+  printf("\nsize of msg: %zu\n", sizeof m);
   return 0;
 }
